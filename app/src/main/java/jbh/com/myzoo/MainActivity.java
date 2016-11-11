0c4d5404846ba4869c551a41f74ce9dd4a1ea302
jbt <bdhbsh@fsdfd.com>
2016-11-11 10:07:22 +0200

    inital upload

---
+++ app/src/main/java/jbh/com/myzoo/MainActivity.java
@@ -0,0 +1,22 @@
+package jbh.com.myzoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yossi.zoo.Dolphin;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Dolphin d= new Dolphin();
+        d.swim();
+
+
+
+    }
+}
