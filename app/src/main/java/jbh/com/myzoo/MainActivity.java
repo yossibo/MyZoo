735b18172048ba667c8d45dc4ad7b44dc195cdb9
jbt <bdhbsh@fsdfd.com>
2016-11-11 10:12:18 +0200

    added log

--- app/src/main/java/jbh/com/myzoo/MainActivity.java
+++ app/src/main/java/jbh/com/myzoo/MainActivity.java
@@ -2,6 +2,7 @@ package jbh.com.myzoo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.yossi.zoo.Dolphin;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("dsd","dsdsds");
 
 
         Dolphin d= new Dolphin();
